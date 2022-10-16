d3da6c7b51d98191b454fd8454399ce3e5449f2b
Andrew <dubofweek@gmail.com>
2022-10-16 18:13:22 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Сколько вам лет?', '');
+// answers[2] = prompt('Вы мужчина?', '');
+
+// console.log(answers);
+
+// const category = 'toys';
+
+// console.log(`https://www.someurl.com/${category}/5`);
+
+// const user = 'Оля';
+
+// alert(`Привет, ${user}`);
