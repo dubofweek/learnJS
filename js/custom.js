caf5cb65cbb751631adee4f80cd1cb156fce7328
Andrew <dubofweek@gmail.com>
2022-10-30 14:47:03 +0200

    learnJS first part is finished

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,143 @@
-'use strict';
+// ****************console.log***************
+// console.log(userName);
+// console.log(77);
 
-for (let i = 0; i <= 10; i++) {
-    if (i%2 == 0) {
-        continue;
-    }
-    console.log(i);
-}
+// *****************variables***********************
+// var userName = 'Anthony';
+// console.log(userName);
+// userName = 'Jade';
+// console.log(userName);
+// let color; 
+// console.log(color);
+// color = 'red';
+// console.log(color); 
 
+/*******************************************************
+JS variables names can include:
+letters, numbers, $, _myName
+JS variables names can't start with number:
+1color
+*******************************************************/
+
+// var myMainColor = 'gReen'; //camel case
+// var my_main_color = 'gr_een'; //snake case
+// var my-main-color = 'gre-een'; //kebab case
+// var MyMainColor = 'Green'; //Pascal cace
+
+// const myName = 'Andrew';
+// console.log(myName);
+// myName = 'Jane'; - error
+
+// const myBio = {
+//     name: 'Andrew',
+//     gender: 'male',
+//     age: 34
+// };
+// console.log(myBio);
+// myBio.age = 35;
+// console.log(myBio);
+
+// const users = ['Jack', 'Jane', 'Igor'];
+// console.log(users);
+// users.push(17);
+// console.log(users)
+
+// ************Primitive data types************************
+// *String
+// const someText = 'Some Text';
+// console.log(typeof(someText));
+// *Number
+// const someNumber = 11;
+// console.log(typeof(someNumber));
+// *Boolean
+// const someBoolean = true;
+// console.log(typeof(someBoolean));
+// *null
+// const someNull = null;
+// console.log(typeof(someNull));
+// *undefined
+// let someUndefined;
+// console.log(typeof(someUndefined));
+// *Symbol
+// const someSymbol = Symbol();
+// console.log(typeof(someSymbol));
+
+// *************Reference data types - get back object**********
+// *Array
+// const someArray = [1, 344, 343];
+// console.log(typeof(someArray));
+// *Object literal
+// const someObjectLiteral = {
+//     firstProperty: '12',
+//     secondProperty: 23
+// };
+// console.log(typeof(someObjectLiteral));
+// *Function
+// const someFunction = new Function();
+// console.log(typeof(someFunction));
+// *Date
+// const someDate = new Date();
+// console.log(typeof(someDate));
+
+// ****************Conversion to String*****************
+// *String()
+// let x = String(1131);
+// x = String(2 + 2);
+// x = String(false);
+// x = String(new Date());
+// x = String(['one', 'two', 'three', 10, true, 144]);
+
+// *toString()
+// x = (23).toString();
+
+// console.log(x);
+// console.log(typeof(x));
+// console.log(x.length);
+
+// **************Conversion to Number*****************
+// *Number()
+// let y = Number('5.123445');
+// y = Number(false);
+// y = Number(null);
+// y = Number('text');
+// y = Number([1, 4, 6]);
+
+// *parseInt()
+// y = parseFloat('123.40');
+
+// console.log(y);
+// console.log(typeof y);
+// console.log(y.toPrecision(5));
+
+// const x = '5';
+// const y = '3';
+// const z = x & y;
+
+// console.log(z);
+// console.log(typeof z);
+
+// ************Numbers Math**********************
+// const firstNumber = 50;
+// const secondNumber = 3;
+// let result;
+// *****Simple math operations***
+// result = firstNumber + secondNumber;
+// result = firstNumber - secondNumber;
+// result = firstNumber / secondNumber;
+// result = firstNumber % secondNumber;
+
+// *****Math object**********
+// result = Math.PI;
+// result = Math.round(3.5);
+// result = Math.ceil(3.3);
+// result = Math.floor(3.7);
+// result = Math.sqrt(100);
+// result = Math.abs(-7);
+// result = Math.pow(5, 4);
+// result = Math.min(3, 45, 76, 89, 23);
+// result = Math.max(3, 45, 76, 89, 23);
+// result = Math.round(Math.random() * 100) + 1;
+// console.log(result);
+
+
+ // ***String****
